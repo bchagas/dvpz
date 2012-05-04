2482cc950adb8464459c7f850f8c5d36fa2dc0b4
Bruno Chagas <brunochagasweb@gmail.com>
2012-05-04 16:01:39 -0300

    Added compass configuration

--- app.rb
+++ app.rb
@@ -2,10 +2,24 @@ require "bundler"
 Bundler.setup(:default, ENV.fetch("RACK_ENV", :development))
 Bundler.require
 
+configure do
+  Compass.configuration do |config|
+    config.project_path = File.dirname(__FILE__)
+    config.sass_dir = 'views'
+  end
+
+  set :haml, { :format => :html5 }
+  set :sass, Compass.sass_engine_options
+end
+
 get '/:file.css' do
   sass :"#{params[:file]}"
 end
 
 get '/' do
   haml :index
+end
+
+get '/about' do
+  haml :about
 end
